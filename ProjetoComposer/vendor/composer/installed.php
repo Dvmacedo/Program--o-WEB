f9767caba8ee49451d861b614a85da65da4c4581
Dvmacedo <diego.santos209@fatec.sp.gov.br>
2024-03-16 11:50:07 -0300

    init

---
+++ ProjetoComposer/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'php/primeiroprojeto',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '476d10c444c7bc1dcd1f6e50dbb9659462632f57',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'php/primeiroprojeto' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '476d10c444c7bc1dcd1f6e50dbb9659462632f57',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
