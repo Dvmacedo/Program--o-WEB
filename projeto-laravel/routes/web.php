87ba9c716ca3657706f7a2080057f93f702f0834
Dvmacedo <diego.santos209@fatec.sp.gov.br>
2024-05-11 12:21:51 -0300

    exer1

--- projeto-laravel/routes/web.php
+++ projeto-laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function(){
     return view('welcome');
+});
+
+Route::get('/bem-vindo', function(){
+    return "Seja bem vindo!";
+});
+
+Route::get('/exer1', function(){
+    return view('exer1');
+});
+
+Route::post('/exer1resp', function(Request $request){
+    $valor = $request->input('valor1');
+    return $valor;
 });
